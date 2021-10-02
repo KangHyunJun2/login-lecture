14b067a37745465357847dc879c25b8e036f540b
KangHyunJun2 <sktyum13@sau.ac.kr>
2021-10-02 23:40:50 +0900

    최종 최적화

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,12 +1,15 @@
 "use strict"
 
 const id = document.querySelector("#id"),
-psword = document.querySelector("#psword"),
-loginBtn = document.querySelector("#button");
+    psword = document.querySelector("#psword"),
+    loginBtn = document.querySelector("#button");
 
 loginBtn.addEventListener("click", login);
 
 function login() {
+    if (!id.value) return alert("아이디를 입력해 주십시오");
+    if (!psword.value) return alert("비밀번호를 입력해 주십시오");
+
     const req = {
         id: id.value,
         psword: psword.value,
@@ -19,15 +22,16 @@ function login() {
         },
         body: JSON.stringify(req),
     })
-    .then((res) => res.json())
-    .then((res) => {
-        if (res.success) {
-            location.href = "/";
-        } else {
-            alert(res.msg);
-        }
-    })
-    .catch((err) => {
-        console.error("로그인 중 에러 발생");
-    })
+        .then((res) => res.json())
+        .then((res) => {
+            if (res.success) {
+                location.href = "/";
+            } else {
+                if (res.err) return alert(res.err);
+                alert(res.msg);
+            }
+        })
+        .catch((err) => {
+            console.error("로그인 중 에러 발생");
+        })
 }
--- app/src/public/js/home/register.js
+++ app/src/public/js/home/register.js
@@ -1,16 +1,16 @@
 "use strict"
 
 const id = document.querySelector("#id"),
-name = document.querySelector("#name"),
-psword = document.querySelector("#psword"),
-confirmPsword = document.querySelector("#confirm-psword"),
-registerBtn = document.querySelector("#button");
+    name = document.querySelector("#name"),
+    psword = document.querySelector("#psword"),
+    confirmPsword = document.querySelector("#confirm-psword"),
+    registerBtn = document.querySelector("#button");
 
 registerBtn.addEventListener("click", register);
 
 function register() {
-    if (!id.value) return alert("아이디를 입력해 주십시오.");
-    if (psword.value !== confirmPsword.value) return alert("비밀번호가 일치하지 않습니다.");
+    if (!id.value) return alert("아이디를 입력해 주십시오");
+    if (psword.value !== confirmPsword.value) return alert("비밀번호가 일치하지 않습니다");
 
     const req = {
         id: id.value,
@@ -25,15 +25,16 @@ function register() {
         },
         body: JSON.stringify(req),
     })
-    .then((res) => res.json())
-    .then((res) => {
-        if (res.success) {
-            location.href = "/login";
-        } else {
-            alert(res.msg);
-        }
-    })
-    .catch((err) => {
-        console.error("회원가입 중 에러 발생");
-    })
+        .then((res) => res.json())
+        .then((res) => {
+            if (res.success) {
+                location.href = "/login";
+            } else {
+                if (res.err) return alert(res.err);
+                alert(res.msg);
+            }
+        })
+        .catch((err) => {
+            console.error("회원가입 중 에러 발생");
+        })
 }
